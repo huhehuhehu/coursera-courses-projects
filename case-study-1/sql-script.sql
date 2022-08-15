150a0213a3439c8aa625f75a3d4c8418e40cd306
huhehuhehu <70438266+huhehuhehu@users.noreply.github.com>
2022-08-15 18:22:40 +0700

    added VAR(duration) in SQL

--- case-study-1/sql-script.sql
+++ case-study-1/sql-script.sql
@@ -41,10 +41,10 @@ DELETE FROM tempo
 WHERE duration < 5 OR duration >600 OR CONCAT(duration, member_casual) IS NULL;
 
 /*aggregate the data to useful ones only*/
-SELECT DATE_FORMAT(started_at, '%Y-%m-01') AS mon, member_casual, ROUND(AVG(duration),2) AS average_duration, COUNT(*) AS total_rides, MIN(duration), MAX(duration)
+SELECT DATE_FORMAT(started_at, '%Y-%m-01') AS mon, member_casual, ROUND(AVG(duration),2) AS average_duration, ROUND(VAR(duration),2) AS var_dur, COUNT(*) AS total_rides, MIN(duration), MAX(duration)
 FROM tempo
 GROUP BY MONTH(started_at), member_casual;
 
-SELECT DATE_FORMAT(started_at, '%W') AS weekday, member_casual, ROUND(AVG(duration),2) AS average_duration, COUNT(*) AS total_rides, MIN(duration), MAX(duration)
+SELECT DATE_FORMAT(started_at, '%W') AS weekday, member_casual, ROUND(AVG(duration),2) AS average_duration, ROUND(VAR(duration),2) AS var_dur, COUNT(*) AS total_rides, MIN(duration), MAX(duration)
 FROM tempo
 GROUP BY WEEKDAY(started_at), member_casual;
